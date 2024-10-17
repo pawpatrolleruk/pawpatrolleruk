29226011486706e9150aaa911d2f20fede2dcba7
pawpatrolleruk <orchesterlover1@gmail.com>
2024-10-17 20:39:37 +0200

    Initialize

---
+++ js/navbar.js
@@ -0,0 +1,1 @@
+  /* When the screen is less than 600 pixels wide, hide all links, except for the first one ("Home"). Show the link that contains should open and close the topnav (.icon) */
---
+++ js/script.js
@@ -0,0 +1,5 @@
+// js/script.js
+// function toggleMenu() {
+//     const headerButtons = document.querySelector('.header-buttons');
+//     headerButtons.classList.toggle('show');
+// }
